150d748359e77c678b635e808b0f82b54beb3ab8
kastnew <hieuphamduy17@gmail.com>
2025-12-15 20:41:14 +0700

    Initial commit

---
+++ Group-OOP/src/main/java/Group/OOP/Group/OOP/controller/DailySummaryController.java
@@ -0,0 +1,24 @@
+package Group.OOP.Group.OOP.controller;
+
+import Group.OOP.Group.OOP.entity.DailySummary;
+import Group.OOP.Group.OOP.service.DailySummaryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/dailysummary")
+public class DailySummaryController {
+
+    @Autowired
+    DailySummaryService dailySummaryService;
+
+    // lấy daily summary của 1 người trong 1 ngày
+    @PostMapping("/{id}")
+    public DailySummary getDailySummary(@PathVariable Integer id , @RequestBody LocalDate date) {
+        return dailySummaryService.findFirstByUserIdAndDate(id , date) ;
+    }
+}
---
+++ Group-OOP/src/main/java/Group/OOP/Group/OOP/controller/MonthSummaryController.java
@@ -0,0 +1,22 @@
+package Group.OOP.Group.OOP.controller;
+
+import Group.OOP.Group.OOP.dto.MonthSummary;
+import Group.OOP.Group.OOP.service.MonthSummaryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/monthsummary")
+public class MonthSummaryController {
+    @Autowired
+    private MonthSummaryService monthSummaryService;
+
+    @PostMapping("/{id}")
+    public MonthSummary getMonthSummary(@PathVariable Integer id , @RequestBody Integer month) {
+        return monthSummaryService.monthSummary(id, month);
+    }
+}
